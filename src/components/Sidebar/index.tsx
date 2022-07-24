9b8cf04394e12b20ce94535182ee1c2e3635ab36
Jonas Tranberg <jonastranberg93@gmail.com>
2022-07-24 20:55:30 +0200

    Sidebar changes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
-import { Button, Chip, Divider, Paper, styled, useTheme } from "@mui/material";
+import { Button, Chip, Divider, Link, Paper, styled, useTheme } from "@mui/material";
 import { FunctionComponent } from "react";
-import { AiOutlineTeam } from "react-icons/ai";
-import { IoBuildOutline, IoCallOutline, IoPulseOutline } from "react-icons/io5";
+import { AiOutlineGithub, AiOutlineTeam } from "react-icons/ai";
+import { IoBookOutline, IoBuildOutline, IoCallOutline, IoHelpCircleOutline, IoPulseOutline } from "react-icons/io5";
 import { GoBrowser, GoGraph } from "react-icons/go";
 import { RiAlarmWarningLine } from "react-icons/ri";
 import { TbComponents } from "react-icons/tb";
@@ -23,7 +23,7 @@ const SidebarContainer = styled(Paper)(({ theme }) => ({
 }));
 
 const Sidebar: FunctionComponent = () => {
-    const user = useUser(state => state.user);
+    const user = useUser((state) => state.user);
     const theme = useTheme();
     const iconColor = theme.palette.info.main;
 
@@ -37,6 +37,18 @@ const Sidebar: FunctionComponent = () => {
             <SidebarItem to="/reports" text="Reports" icon={<GoGraph color={iconColor} />} />
             <SidebarItem to="/people" text="People" icon={<AiOutlineTeam color={iconColor} />} />
             <SidebarItem to="/integrations" text="Integrations" icon={<TbComponents color={iconColor} />} />
+
+            <SidebarDivider />
+
+            <SidebarLinkItem icon={<AiOutlineGithub />} href="https://github.com/opsway-io" text="GitHub" />
+            <SidebarLinkItem icon={<IoBookOutline />} href="https://docs.opsway.io" text="Documentation" />
+
+            <Divider
+                sx={{
+                    marginTop: 2,
+                    marginBottom: 2,
+                }}
+            />
 
             <SidebarActions picture={user?.picture} name={user?.displayName} />
         </SidebarContainer>
@@ -60,25 +72,48 @@ const SidebarItem: FunctionComponent<SidebarItemProps> = (props) => {
                 fontWeight: 500,
                 fontSize: (theme) => theme.typography.body1.fontSize,
                 padding: (theme) => theme.spacing(1),
-                marginBottom: (theme) => theme.spacing(1),
+                marginBottom: "4px",
                 "&.active": {
-                    backgroundColor: theme => theme.palette.grey[200],
+                    backgroundColor: (theme) => theme.palette.grey[200],
                     fontWeight: 600,
-                }
+                },
             }}
             component={NavLink}
             to={props.to}
         >
             {props.text}
-            {props.count && <Chip color="info" style={{ marginLeft: "auto", minWidth: 32 }} label={props.count} size="small" />}
+            {props.count && (
+                <Chip color="info" style={{ marginLeft: "auto", minWidth: 32 }} label={props.count} size="small" />
+            )}
         </Button>
     );
 };
 
+interface SidebarLinkItemProps {
+    text: string;
+    icon: React.ReactNode;
+    href: string;
+}
+
+const SidebarLinkItem: FunctionComponent<SidebarLinkItemProps> = (props) => {
+    return (
+        <Link
+            component={Button}
+            startIcon={props.icon}
+            href={props.href}
+            target="_blank"
+            sx={{
+                justifyContent: "flex-start",
+                textDecoration: "none",
+                opacity: 0.5,
+            }}
+        >
+            {props.text}
+        </Link>
+    );
+};
+
 const SidebarActionsContainer = styled("div")(({ theme }) => ({
-    borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: "auto",
-    paddingTop: theme.spacing(2),
     display: "flex",
 }));
 
@@ -109,6 +144,16 @@ const SidebarActions: FunctionComponent<SidebarActionsProps> = (props) => {
                 {props.name}
             </Button>
         </SidebarActionsContainer>
+    );
+};
+
+const SidebarDivider: FunctionComponent = () => {
+    return (
+        <div
+            style={{
+                margin: "auto",
+            }}
+        />
     );
 };
 

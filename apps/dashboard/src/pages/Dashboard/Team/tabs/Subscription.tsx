bb0baaf335864b2c7cca71620d905f3af4acd26e
teisnp <teis.n.p@gmail.com>
2024-02-05 19:59:27 +0100

    generic lookupkeys for subscriptions

--- apps/dashboard/src/pages/Dashboard/Team/tabs/Subscription.tsx
+++ apps/dashboard/src/pages/Dashboard/Team/tabs/Subscription.tsx
@@ -47,7 +47,7 @@ const TeamPlanTabView: FunctionComponent = () => {
                   ]}
                   selected={team?.paymentPlan === "FREE"}
                   teamId={team?.id!}
-                  priceLookupKey={""}
+                  priceLookupKey={"FREE"}
                 />
               </Grid>
               <Grid item>
@@ -64,7 +64,7 @@ const TeamPlanTabView: FunctionComponent = () => {
                   ]}
                   selected={team?.paymentPlan === "TEAM"}
                   teamId={team?.id!}
-                  priceLookupKey={"price_1NjhTUAAd26uMXu2JFv0zKjd"}
+                  priceLookupKey={"TEAM"}
                 />
               </Grid>
               <Grid item>
@@ -82,7 +82,7 @@ const TeamPlanTabView: FunctionComponent = () => {
                   ]}
                   selected={team?.paymentPlan === "ENTERPRISE"}
                   teamId={team?.id!}
-                  priceLookupKey={"price_1NjhXOAAd26uMXu2InlkMmDS"}
+                  priceLookupKey={"ENTERPRISE"}
                 />
               </Grid>
             </Grid>
